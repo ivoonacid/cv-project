e58424560ae1393bbd5234b0ebe735f227859fb8
Ivo Alves <91916188+ivo420alves@users.noreply.github.com>
2022-07-03 23:48:45 +0100

    added basic styling

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>CV App</h1>
+      <h3>General Information</h3>
       <General data={data} setData={setData} />
+      <h3>School Information</h3>
       <Educational data={data} setData={setData} />
+      <h3>Practical Information</h3>
       <Practical data={data} setData={setData} />
       <button onClick={() => console.log(data)}>Send Data</button>
     </div>
